9961c649cd7ab0f057235d5a7430f263b40c765d
imollov <ivomollov@gmail.com>
2026-01-16 11:31:00 +0200

    create job

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import pinoHttp from 'pino-http';
 import logger from './utils/logger';
 import healthRouter from './routes/health';
+import jobsRouter from './routes/jobs';
 
 const app = express();
 
+app.use(express.json());
 app.use(pinoHttp({ logger }));
 
 app.use('/health', healthRouter);
+app.use('/jobs', jobsRouter);
 
 export default app;
