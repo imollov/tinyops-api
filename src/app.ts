864393edd3f366db2cbf1378232bea0d6d4749ce
imollov <ivomollov@gmail.com>
2026-01-16 11:48:49 +0200

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import pinoHttp from 'pino-http';
+import cors from 'cors';
 import { logger } from './utils/logger';
 import { healthRouter } from './routes/health';
 import { jobsRouter } from './routes/jobs';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(pinoHttp({ logger }));
 
